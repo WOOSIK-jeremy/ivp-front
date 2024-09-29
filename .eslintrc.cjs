cb9069cc5b49477f08fc7d7210b46db8db0c5b82
gloomydumber <chromacrow@naver.com>
2024-09-29 18:18:03 +0900

    init : initialize project

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'react-refresh', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ readme.md
@@ -0,0 +1,6 @@
+# ivp-front
+
+This project follows Google's Commit Message Guide for commit conventions and adheres to the Airbnb JavaScript/TypeScript Style Guide.
+
++ [Commit Message Convention](https://developers.google.com/blockly/guides/contribute/get-started/commits)
++ [Airbnb JavaScript Style Guide](https://github.com/airbnb/javascript)
